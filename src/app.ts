4b6f9477dc0ab3dc5cf4a6677ed78fd3c2bd1192
Ritesh7767 <ritesh776782@gmail.com>
2025-05-18 13:20:44 +0530

    updating frontend url

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://schoolmanag.netlify.app",
     credentials: true
 }))
 app.get("/", (_, res) => {
